858aefd807ece18a28619161fdd52b98fd446a0d
Sophia Leal <sophialeal@eaportal.org>
2025-05-12 11:13:54 -0300

    Exemplo 4 e 5

--- main.js
+++ main.js
@@ -19,9 +19,36 @@ somar(n1,n2)
 somar("14",18)*/
 
 //Exemplo 3 - paramêtros de tipos diferentes
-function mensagemPessoal(nome, cargo, anosTrabalhados) {
+/*function mensagemPessoal(nome, cargo, anosTrabalhados) {
     alert('Seja bem vindo(a) ${cargo} $ nome. Estamos felizez por ter você trabalhando conosco há ${anosTrabalhados} anos.')
 }
 
 mensagemPessoal("Lucas", "Gerente", 10)
-mensagemPessoal("Irineu", "Você não sabe, nem eu", 50)
+mensagemPessoal("Irineu", "Você não sabe, nem eu", 50)*/
+
+//Exemplo 4 - o return nas funções
+/*function somarEDevolver(numero1, numero2, numero3) {
+    let resultado = numero1 + numero2 + numero3
+    return resultado
+}
+
+let soma1 = somarEDevolver(10, 11, 12)
+let soma2 = somarEDevolver(30, 40, 50)
+
+alert(soma1)
+alert(soma2)*/
+
+//Exemplo 5 - if sem o else 
+/*function parOuImpar(numero) {
+    if (numero % 2 === 0){
+        return "PAR"
+    }
+    return "ÍMPAR"
+
+    let resultado1 = parOuImpar(10)
+    let resultado2 = parOuImpar(15)
+
+    alert('O número 10 é ${resultado1}')
+    alert('O número 10 é ${resultado1}')
+}*/
+
